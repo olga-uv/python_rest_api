f9eb43feaf97876e5025b1a4a91d069cff60aed4
Uvarova <Olga.Uvarova@emc.com>
2022-05-14 00:35:38 +0300

    json

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ def test_response_code():
     response = requests.get('https://regres.in/api/users?page=2')
 
     assert response.status_code == 200
-
-
 def test_response_code_2():
     response = requests.get('https://reqres.in/api/users?page=2')
 
@@ -22,14 +20,23 @@ def test_response_code_2():
 user_data = {"name": "morpheus2", "job": "leader2"}
 
 
-response = requests.post('https://reqres.in/api/users', data=user_data)
-print(response.status_code)
-print(type(response))
-print(response.json())
+#response = requests.post('https://reqres.in/api/users', data=user_data)
+#print(response.status_code)
+#print(type(response))
+#print(response.json())
 
 
 def test_post_request():
     response3 = requests.post('https://reqres.in/api/users', data=user_data)
 
     assert response3.status_code == 201
+
+
+def test_get_users():
+    response = requests.get('https://reqres.in/api/users?page=2')
+    parsed_response = response.json()
+    print(parsed_response['data'][2]['email'])
+    assert parsed_response['data'][2]['email'] == "tobias.funke@reqres.in"
+    assert len(parsed_response['data']) == 6
+
 
