2e5a358f4fa6fb66581c9abd9daf475b72908a54
Uvarova <Olga.Uvarova@emc.com>
2022-05-27 23:57:24 +0300

    added get user test from bitly

---
+++ bitly.py
@@ -0,0 +1,16 @@
+import requests
+import pytest
+
+base_url = 'https://api-ssl.bitly.com/v4/'
+token = 'f53e63a197998dc12679944b467c81df719bdd28'
+user_email = 'leonteva.h@gmail.com'
+
+
+def test_get_user_profile():
+    headers = {"Authorization": f'Bearer {token}'}
+    response = requests.get(f'{base_url}user', headers=headers)
+    print(response.text)
+    print(response.status_code)
+
+    assert response.json()['emails'][0]['email'] == user_email
+
